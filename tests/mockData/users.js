b6487f5abf98e1fa01b81955d0a5ce4b16b1f6da
FidelVe <43641727+FidelVe@users.noreply.github.com>
2024-06-10 15:45:38 -0400

    updated mock data

--- tests/mockData/users.js
+++ tests/mockData/users.js
@@ -1,9 +1,18 @@
 //
+const mongoose = require("mongoose");
+
 const users = [
   {
     walletAddress: "0x1234567890",
     registrationBlock: 1,
     updatedAtBlock: 2,
+    seasons: [
+      {
+        seasonId: new mongoose.Types.ObjectId(),
+        registrationBlock: 1,
+        referrals: [],
+      },
+    ],
   },
   {
     walletAddress: "0x0987654321",
