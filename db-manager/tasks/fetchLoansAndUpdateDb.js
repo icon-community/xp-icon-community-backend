ab54c7645418b1e52169fc0e4d161e1c3f10dffd
FidelVe <43641727+FidelVe@users.noreply.github.com>
2024-06-21 08:06:57 -0400

    fixed issue. after depositing collateral with AVAX, loans were not being properly fetch

--- db-manager/tasks/fetchLoansAndUpdateDb.js
+++ db-manager/tasks/fetchLoansAndUpdateDb.js
@@ -3,13 +3,13 @@
 // task id in the seed labeled as "t2" (seedId === "t2");
 const SEED_ID = "t2";
 const genericTask = require("./genericTask");
-const { getSicxDebtInUSDValue } = require("../utils/json-rpc-services");
+const { getTotalDebtInUSDValue } = require("../utils/json-rpc-services");
 
 async function fetchLoansAndUpdateDb(taskInput, db) {
   try {
     console.log("========");
     console.log("> Running fetchLoansAndUpdateDb task");
-    return await genericTask(taskInput, db, SEED_ID, getSicxDebtInUSDValue);
+    return await genericTask(taskInput, db, SEED_ID, getTotalDebtInUSDValue);
   } catch (err) {
     console.log("Error running fetchLoansAndUpdateDb task");
     console.log(err);
