29e9482d722ca2bc3c4ab785bff49601c3a34e30
MarkacRobi <robi.markac@gmail.com>
2024-09-27 10:23:58 +0200

    feat(direct-referral): endpoint to create user referral directly

--- nestjs-rest-server/src/user/decorator/user.decorators.ts
+++ nestjs-rest-server/src/user/decorator/user.decorators.ts
@@ -1,6 +1,11 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const UserAddress = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
+
+  if (!request.publicAddress) {
+    throw new BadRequestException("publicAddress undefined in request");
+  }
+
   return request.publicAddress;
 });
