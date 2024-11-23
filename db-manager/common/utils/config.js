9e3b49fbbe6decf62f9f1e57fde5eefb6b9644b5
FidelVe <43641727+FidelVe@users.noreply.github.com>
2024-11-23 15:46:54 -0400

    chore: refactored docker compose files

--- db-manager/common/utils/config.js
+++ db-manager/common/utils/config.js
@@ -39,7 +39,6 @@ const config = {
     lineBreak: "\n------------------------------------",
   },
   ports: {
-    backend: process.env.REST_PORT,
     dbManager: process.env.DB_MANAGER_PORT,
   },
   collections: {
@@ -47,6 +46,7 @@ const config = {
     task: process.env.TASK_COLLECTION,
     season: process.env.SEASON_COLLECTION,
     userTask: process.env.USER_TASK_COLLECTION,
+    referrals: process.env.REFERRALS_COLLECTION,
   },
   db: {
     user: process.env.MONGO_USER,
@@ -55,9 +55,7 @@ const config = {
     dbName: process.env.MONGO_DB_NAME,
     containerName: process.env.MONGO_CONTAINER,
   },
-  flags: {
-    useMockDb: process.env.USE_MOCK_DB,
-  },
+  flags: {},
   jvm: {
     routes: {
       v3: "/api/v3",
