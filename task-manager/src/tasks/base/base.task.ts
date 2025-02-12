8b52357030bb4e849f813a38334b548c4e5f640e
FidelVe <43641727+FidelVe@users.noreply.github.com>
2025-02-12 14:11:09 -0400

    refactored base.task.ts file

--- task-manager/src/tasks/base/base.task.ts
+++ task-manager/src/tasks/base/base.task.ts
@@ -2,13 +2,17 @@ import { Logger } from "@nestjs/common";
 import { TaskInput } from "../../shared/types/GeneralTypes";
 import { UserTaskStatus } from "../../shared/enum/general-enum";
 import { SeasonsService } from "../../collections/seasons/seasons.service";
+import { SeasonDocument } from "../../collections/seasons/schemas/seasons.schema";
 import { TasksService } from "../../collections/tasks/tasks.service";
+import { TaskDocument } from "../../collections/tasks/schemas/tasks.schema";
 import { UsersService } from "../../collections/users/users.service";
+import { UserDocument } from "../../collections/users/schemas/users.schema";
 import { UserTasksService } from "../../collections/user-tasks/user-tasks.service";
+import { UserTasksDocument } from "../../collections/user-tasks/schemas/user-tasks.schema";
 import { XpEarned } from "../../collections/user-tasks/schemas/user-tasks.schema";
 import { Types } from "mongoose";
 
-export class BaseTask {
+export abstract class BaseTask {
   readonly logger: Logger;
   protected readonly seasonsService: SeasonsService;
   protected readonly tasksService: TasksService;
@@ -28,193 +32,200 @@ export class BaseTask {
     this.userTasksService = userTasksService;
   }
 
-  async execute(
-    taskInput: TaskInput,
-    callback: (arg: TaskInput) => Promise<XpEarned>,
-    taskType: string,
-  ): Promise<void> {
+  async execute(taskInput: TaskInput): Promise<void> {
     try {
-      this.logger.log({
-        level: "info",
-        message: `${this.constructor.name} begin execution. Task Input: ${JSON.stringify(taskInput)}`,
-      });
+      this.logTaskStart(taskInput);
 
-      // Fetch the target task from the database
-      const targetTask = await this.tasksService.findBySeedId(taskType);
+      const targetTask = await this.validateTask();
+      if (!targetTask) return;
 
-      // validate the task document
-      if (!targetTask) {
-        this.logger.log({
-          level: "info",
-          message: `${this.constructor.name} task ${taskType} not found in the database. Task execution skipped.`,
-        });
-        return;
-      }
+      const activeSeasons = await this.findActiveSeasons();
+      if (!activeSeasons.length) return;
 
-      // Fetch active seasons from the database
-      const activeSeasons = await this.seasonsService.findActiveSeasons();
-
-      if (activeSeasons.length === 0) {
-        this.logger.log({
-          level: "info",
-          message: `${this.constructor.name} no active seasons found. Task execution skipped.`,
-        });
-        return;
-      }
-
-      // Iterate over each active season
-      for (const season of activeSeasons) {
-        // if the current block is not within the season's block range, skip the season
-        if (
-          taskInput.height < season.blockStart ||
-          taskInput.height > season.blockEnd
-        ) {
-          this.logger.log({
-            level: "info",
-            message: `${this.constructor.name} block height ${taskInput.height} is outside of season ${season._id} block range. Task execution skipped.`,
-          });
-          continue;
-        }
-        // Fetch the tasks for this active season
-        const tasks = season.tasks;
-
-        // Search for the taskType to be executed inside the
-        // tasks in the active season
-        const taskIsInSeason = tasks.some(
-          (taskId) => taskId.toString() === targetTask._id.toString(),
-        );
-
-        if (!taskIsInSeason) {
-          this.logger.log({
-            level: "info",
-            message: `${this.constructor.name} task ${taskType} not found in active season. Task execution skipped.`,
-          });
-          continue;
-        }
-
-        // Fetch all the users in this season
-        const allUsers = await this.usersService.findUsersBySeason(
-          new Types.ObjectId(season._id?.toString()),
-        );
-
-        if (allUsers.length === 0) {
-          this.logger.log({
-            level: "info",
-            message: `${this.constructor.name} no users found in season ${season._id}. Task execution skipped.`,
-          });
-          continue;
-        }
-
-        // Iterate over each user in the season
-        for (const currentUser of allUsers) {
-          // Check user registration agains the current
-          // block
-          const seasonRegistrationData = currentUser.seasons.find(
-            (seasonRegistrationInfo) => {
-              return (
-                seasonRegistrationInfo.seasonId.toString() ===
-                season._id.toString()
-              );
-            },
-          );
-          if (
-            seasonRegistrationData.registrationBlock == null ||
-            seasonRegistrationData.registrationBlock > taskInput.height
-          ) {
-            this.logger.log({
-              level: "info",
-              message: `${this.constructor.name} user ${currentUser._id} has an invalid registrationBlock (${seasonRegistrationData.registrationBlock}) or current block height is lower than user registrationBlock for season ${season._id}. Task execution skipped.`,
-            });
-            continue;
-          }
-
-          try {
-            await callback(
-              taskInput,
-              currentUser,
-              season,
-              targetTask,
-              // userDocument,
-              // seasonDocument,
-              // taskDocument,
-            );
-          } catch (err) {
-            this.logger.error({
-              level: "error",
-              message: `${this.constructor.name} error: ${err.message}`,
-              error: err,
-            });
-          }
-
-          // // initialize user-task data
-          // const userTasksData = {
-          //   userId: new Types.ObjectId(currentUser._id.toString()),
-          //   taskId: new Types.ObjectId(targetTask._id.toString()),
-          //   seasonId: new Types.ObjectId(season._id.toString()),
-          //   xpEarned: [],
-          //   status: UserTaskStatus.PENDING,
-          //   walletAddress: currentUser.walletAddress,
-          // };
-
-          // // Fetch the current user-task document
-          // const currentUserTaskDocument =
-          //   await this.userTasksService.findByAllIds(
-          //     userTasksData.userId,
-          //     userTasksData.taskId,
-          //     userTasksData.seasonId,
-          //   );
-
-          // let xpObj = {
-          //   period: taskInput.prepTerm,
-          //   xp: 0,
-          //   block: taskInput.height,
-          // };
-          // try {
-          //   // Execute custom task logic from child class
-          //   // to calculate the new xp
-          //   xpObj = await callback(taskInput);
-
-          //   // TODO push new xp to userTasksData.xpEarned
-          // } catch (err) {
-          //   this.logger.error(
-          //     `Error calculating new XP on task ${this.constructor.name}: ${err.message}`,
-          //   );
-          //   continue;
-          // }
-          // if (currentUserTaskDocument == null) {
-          //   userTasksData.xpEarned.push(xpObj);
-          //   await this.userTasksService.create(userTasksData);
-          // } else {
-          //   // Check if the task has already been executed
-          //   if (currentUserTaskDocument.xpEarned.length > 0) {
-          //     const taskAlreadyExecuted = currentUserTaskDocument.xpEarned.some(
-          //       (xpDoc: XpEarned) => {
-          //         return xpDoc.period === taskInput.prepTerm;
-          //       },
-          //     );
-
-          //     if (taskAlreadyExecuted) {
-          //       this.logger.log({
-          //         // If the task has already been executed, skip the user
-          //         level: "info",
-          //         message: `${this.constructor.name} task ${taskType} already executed for user ${currentUser._id} in season ${season._id}. Task execution skipped.`,
-          //       });
-
-          //       continue;
-          //     }
-          //     // add new xp to the existing user-task document
-          //     await this.userTasksService.addXp(
-          //       currentUserTaskDocument._id,
-          //       xpObj,
-          //     );
-          //   }
-          // }
-        }
-      }
+      await this.processSeasons(activeSeasons, taskInput, targetTask);
     } catch (err) {
       throw new Error(
         `Error executing task ${this.constructor.name}: ${err.message}`,
       );
     }
+  }
+
+  protected abstract getTaskType(): string;
+  protected abstract calculateXp(
+    taskInput: TaskInput,
+    userDocument: UserDocument,
+    seasonDocument: SeasonDocument,
+    taskDocument: TaskDocument,
+  ): Promise<XpEarned>;
+
+  protected logTaskStart(taskInput: TaskInput): void {
+    this.logger.log({
+      level: "info",
+      message: `${this.constructor.name} begin execution. Task Input: ${JSON.stringify(taskInput)}`,
+    });
+  }
+
+  protected handleError(err: Error): void {
+    const message = `${this.constructor.name} error: ${err.message}`;
+    this.logger.error({
+      level: "error",
+      message: message,
+      error: err,
+    });
+
+    throw new Error(message);
+  }
+
+  private async validateTask(): Promise<TaskDocument | null> {
+    const targetTask = await this.tasksService.findBySeedId(this.getTaskType());
+
+    if (!targetTask) {
+      this.logger.log({
+        level: "info",
+        message: `${this.constructor.name} task ${this.getTaskType()} not found in the database. Task execution skipped.`,
+      });
+    }
+
+    return targetTask;
+  }
+
+  private async findActiveSeasons(): Promise<SeasonDocument[]> {
+    const activeSeasons = await this.seasonsService.findActiveSeasons();
+
+    if (activeSeasons.length === 0) {
+      this.logger.log({
+        level: "info",
+        message: `${this.constructor.name} no active seasons found. Task execution skipped.`,
+      });
+    }
+
+    return activeSeasons;
+  }
+
+  private async processSeasons(
+    activeSeasons: SeasonDocument[],
+    taskInput: TaskInput,
+    targetTask: TaskDocument,
+  ): Promise<void> {
+    for (const season of activeSeasons) {
+      if (!this.isBlockInSeasonRange(taskInput.height, season)) {
+        continue;
+      }
+
+      if (!this.isTaskInSeason(targetTask, season)) {
+        continue;
+      }
+
+      await this.processUsersForSeason(season, taskInput, targetTask);
+    }
+  }
+
+  private isBlockInSeasonRange(
+    height: number,
+    season: SeasonDocument,
+  ): boolean {
+    if (height < season.blockStart || height > season.blockEnd) {
+      this.logger.log({
+        level: "info",
+        message: `${this.constructor.name} block height ${height} is outside of season ${season._id} block range. Task execution skipped.`,
+      });
+
+      return false;
+    }
+
+    return true;
+  }
+
+  private isTaskInSeason(task: TaskDocument, season: SeasonDocument): boolean {
+    const taskIsInSeason = season.tasks.some(
+      (taskId) => taskId.toString() === task._id.toString(),
+    );
+
+    if (!taskIsInSeason) {
+      this.logger.log({
+        level: "info",
+        message: `${this.constructor.name} task ${task.seedId} not found in active season. Task execution skipped.`,
+      });
+    }
+
+    return taskIsInSeason;
+  }
+
+  private async processUsersForSeason(
+    season: SeasonDocument,
+    taskInput: TaskInput,
+    targetTask: TaskDocument,
+  ): Promise<void> {
+    const users = await this.usersService.findUsersBySeason(
+      new Types.ObjectId(season._id?.toString()),
+    );
+
+    if (users.length === 0) {
+      this.logger.log({
+        level: "info",
+        message: `${this.constructor.name} no users found in season ${season._id}. Task execution skipped.`,
+      });
+
+      return;
+    }
+
+    for (const user of users) {
+      if (!this.isUserEligible(user, season, taskInput.height)) {
+        continue;
+      }
+
+      try {
+        const xpEarned = await this.calculateXp(
+          taskInput,
+          user,
+          season,
+          targetTask,
+        );
+        await this.processXpEarned(user, season, targetTask, xpEarned);
+      } catch (err) {
+        this.logger.error({
+          level: "error",
+          message: `${this.constructor.name} error processing user ${user._id}: ${err.message}`,
+          error: err,
+        });
+      }
+    }
+  }
+
+  private isUserEligible(
+    user: UserDocument,
+    season: SeasonDocument,
+    height: number,
+  ): boolean {
+    const seasonRegistrationData = user.seasons.find(
+      (seasonRegistrationInfo) => {
+        return (
+          seasonRegistrationInfo.seasonId.toString() === season._id.toString()
+        );
+      },
+    );
+
+    if (
+      seasonRegistrationData.registrationBlock == null ||
+      seasonRegistrationData.registrationBlock > height
+    ) {
+      this.logger.log({
+        level: "info",
+        message: `${this.constructor.name} user ${user._id} has an invalid registrationBlock (${seasonRegistrationData.registrationBlock}) or current block height is lower than user registrationBlock for season ${season._id}. Task execution skipped.`,
+      });
+
+      return false;
+    }
+
+    return true;
+  }
+
+  private async processXpEarned(
+    user: UserDocument,
+    season: SeasonDocument,
+    task: TaskDocument,
+    xpEarned: XpEarned,
+  ): Promise<void> {
+    console.log("TODO");
   }
 }
